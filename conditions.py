8507c5e5e7955207f7fb1f7252b83d7b844ab6fc
Singireddy Geetha Sri Vara Lakshmi <singireddygeethasri@gmail.com>
2026-05-23 20:19:35 +0530

    Fix formatting of conditional return statement

---
+++ conditions.py
@@ -0,0 +1,11 @@
+def friends_in_trouble(j_angry, s_angry):
+    if j_angry == s_angry:
+        return True
+    else:
+        return False
+
+'''
+using conditional statements we have to return true or false 
+if both are angry return True
+else return False
+'''
