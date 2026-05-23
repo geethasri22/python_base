89e600f5c569726e33da2cc7c589fbf1bc7fd218
Singireddy Geetha Sri Vara Lakshmi <singireddygeethasri@gmail.com>
2026-05-23 20:04:55 +0530

    Remove comment for ListNode class definition

---
+++ DelNthFromEnd.py
@@ -0,0 +1,17 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        temp = ListNode(0, head)
+        fast = temp
+        slow = temp
+        for _ in range(n + 1):
+            fast = fast.next
+        while fast:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return temp.next
