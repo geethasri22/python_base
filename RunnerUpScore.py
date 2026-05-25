bb3703b57c16533524f6bc5bf73dcb836fca3bf3
Singireddy Geetha Sri Vara Lakshmi <singireddygeethasri@gmail.com>
2026-05-25 08:15:58 +0530

    Add RunnerUpScore.py to find runner-up score

---
+++ RunnerUpScore.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    
+    # remove the maximum score
+    max_score = max(arr)
+    while max_score in arr:
+        arr.remove(max_score)
+    runner_up = max(arr)
+    print(runner_up)
